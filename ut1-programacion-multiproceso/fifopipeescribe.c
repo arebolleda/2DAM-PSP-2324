2c081ff791bf09a6f52886fbc46ef7d119882723
Alfonso Rebolleda Sánchez <47109499+arebolleda@users.noreply.github.com>
2023-10-25 09:22:24 +0200

    Add files via upload

---
+++ ut1-programacion-multiproceso/fifopipeescribe.c
@@ -0,0 +1,21 @@
+//fifoescribe2.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h> 
+
+
+int main()
+{
+  int fp;
+  char saludo[] = "Un saludo!!!\n";
+  fp = open("FIFO2", 1);
+ 
+  
+  printf("Mandando información al pipe FIFO...\n");
+  write(fp,saludo, strlen(saludo));
+  close(fp);   
+  return 0; 
+}
